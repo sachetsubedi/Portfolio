a74c6d158b2d0feec2f71d0ec36c2c02f22619dc
Sachet Subedi <sachetsubedi001@gmail.com>
2024-08-09 16:08:06 +0545

    mouse follower hidden for small screens

--- scripts/mouseFollower.js
+++ scripts/mouseFollower.js
@@ -1,12 +1,19 @@
-document.addEventListener("mousemove", (e) => {
+if (window.innerWidth > 768) {
+  console.log(window.innerWidth);
+  document.addEventListener("mousemove", (e) => {
+    const follower = document.getElementById("mouseFollower");
+    follower.style.top = e.clientY - 15 + window.scrollY + "px";
+    follower.style.left = e.clientX - 15 + "px";
+  });
+
+  document.addEventListener("click", (e) => {
+    const follower = document.getElementById("mouseFollower");
+    follower.style.backgroundColor = "var(--primary-color)";
+    setTimeout(() => {
+      follower.style.backgroundColor = "transparent";
+    }, 200);
+  });
+} else {
   const follower = document.getElementById("mouseFollower");
-  follower.style.top = e.clientY - 15 + window.scrollY + "px";
-  follower.style.left = e.clientX - 15 + "px";
-});
-document.addEventListener("click", (e) => {
-  const follower = document.getElementById("mouseFollower");
-  follower.style.backgroundColor = "var(--primary-color)";
-  setTimeout(() => {
-    follower.style.backgroundColor = "transparent";
-  }, 200);
-});
+  follower.style.display = "none";
+}
