df1cbb5bbc0c7ef33400c1017530c0d177611a13
Sachet Subedi <sachetsubedi001@gmail.com>
2024-11-21 22:37:12 +0545

    feat: social media icons hover effect

--- src/components/landing/ContactIcons.tsx
+++ src/components/landing/ContactIcons.tsx
@@ -1,37 +1,31 @@
 import { Icon } from "@iconify/react";
-import Link from "next/link";
-import { Card } from "../ui/card";
-
-const contacts: { icon: string; link: string }[] = [
-  {
-    icon: "mdi:github",
-    link: "https://github.sachetsubedi001.com.np",
-  },
-  {
-    icon: "mdi:linkedin",
-    link: "https://ln.sachetsubedi001.com.np",
-  },
-  {
-    icon: "mdi:instagram",
-    link: "https://insta.sachetsubedi001.com.np",
-  },
-];
+import { HoverEffect } from "./CardHoverEffect";
 
 const ContactIcons = () => {
   return (
     <div className="pt-5 flex gap-2 flex-wrap">
-      {contacts.map((contact, index) => {
-        return (
-          <Card
-            key={index}
-            className="w-fit border-2 border-black dark:border-white rounded-sm p-[2px]"
-          >
-            <Link href={contact.link}>
-              <Icon icon={contact.icon} fontSize={20}></Icon>
-            </Link>
-          </Card>
-        );
-      })}
+      <HoverEffect
+        items={[
+          {
+            title: "",
+            description: "",
+            link: "https://github.sachetsubedi001.com.np",
+            icon: <Icon icon={"mdi:github"}></Icon>,
+          },
+          {
+            title: "",
+            description: "",
+            link: "https://ln.sachetsubedi001.com.np",
+            icon: <Icon icon={"mdi:linkedin"}></Icon>,
+          },
+          {
+            title: "",
+            description: "",
+            link: "https://insta.sachetsubedi001.com.np",
+            icon: <Icon icon={"mdi:instagram"}></Icon>,
+          },
+        ]}
+      ></HoverEffect>
     </div>
   );
 };
