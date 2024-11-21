f7623a1e2c467e4ed44c2eedfc6393e218b1d40b
Sachet Subedi <sachetsubedi001@gmail.com>
2024-11-21 14:56:39 +0545

    greetings init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,14 @@
 "use client";
+import Greeting from "@/components/landing/Greeting";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams";
-import { Button } from "@/components/ui/button";
-import { useTheme } from "next-themes";
 
 export default function Home() {
-  const { setTheme } = useTheme();
   return (
     <div>
       <BackgroundBeamsWithCollision>
-        <div>This is simple text</div>
-        <Button
-          onClick={() => {
-            setTheme("light");
-          }}
-        >
-          Set light
-        </Button>
-        <Button
-          onClick={() => {
-            setTheme("dark");
-          }}
-        >
-          Dark
-        </Button>
+        <div className="dark:text-white">
+          <Greeting></Greeting>
+        </div>
       </BackgroundBeamsWithCollision>
     </div>
   );
---
+++ src/components/landing/Greeting.tsx
@@ -0,0 +1,10 @@
+const Greeting = () => {
+  return (
+    <div>
+      <div className="left">Hi, I&apos;m Sachet Subedi</div>
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Greeting;
