1e87fa113a482c88126adeef08316632691e2f51
Sachet Subedi <sachetsubedi001@gmail.com>
2024-11-23 17:02:49 +0545

    progress bar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ModeToggle } from "@/components/landing/ThemeToggle";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import ProgressProvider from "@/providers/progressProvider";
 import { ThemeProvider } from "@/providers/theme-provider";
 import type { Metadata } from "next";
 import "./globals.css";
@@ -17,14 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="relative">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <ModeToggle></ModeToggle>
-          <TooltipProvider>{children}</TooltipProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ProgressProvider></ProgressProvider>
+          <TooltipProvider>
+            <ModeToggle></ModeToggle>
+
+            {children}
+          </TooltipProvider>
         </ThemeProvider>
       </body>
     </html>
--- src/components/landing/ThemeToggle.tsx
+++ src/components/landing/ThemeToggle.tsx
@@ -10,22 +10,44 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Link from "next/link";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger
-        asChild
-        className="absolute top-3 right-10  z-[1000000]"
-      >
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
+      <Link href="/">
+        <Tooltip delayDuration={10}>
+          <TooltipTrigger asChild>
+            <Button
+              variant="outline"
+              size="sm"
+              className="absolute top-3 right-20  z-[1000000] h-9"
+            >
+              <Icon icon="solar:home-2-linear" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent className="font-bold">Home</TooltipContent>
+        </Tooltip>
+      </Link>
+      <Tooltip delayDuration={10}>
+        <TooltipTrigger
+          asChild
+          className="absolute top-3 right-10  z-[1000000]"
+        >
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+        </TooltipTrigger>
+        <TooltipContent className="font-bold">Theme</TooltipContent>
+      </Tooltip>
       <DropdownMenuContent align="end">
         <DropdownMenuItem
           className="font-bold cursor-pointer"
