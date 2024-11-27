924746e3143a5693162133bb57179ba9348e9906
Sachet Subedi <sachetsubedi001@gmail.com>
2024-11-27 19:17:11 +0545

    queryProvider added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ModeToggle } from "@/components/landing/ThemeToggle";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ProgressProvider from "@/providers/progressProvider";
+import QueryProviderWrapper from "@/providers/queryProvider";
 import { ThemeProvider } from "@/providers/theme-provider";
 import type { Metadata } from "next";
 import "./globals.css";
@@ -18,14 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="relative">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <ProgressProvider></ProgressProvider>
-          <TooltipProvider>
-            <ModeToggle></ModeToggle>
-
-            {children}
-          </TooltipProvider>
-        </ThemeProvider>
+        <QueryProviderWrapper>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ProgressProvider></ProgressProvider>
+            <TooltipProvider>
+              <ModeToggle></ModeToggle>
+              {children}
+            </TooltipProvider>
+          </ThemeProvider>
+        </QueryProviderWrapper>
       </body>
     </html>
   );
