59a59fc13ba21ab9d0a968b4aaff555808f76276
Sachet Subedi <sachetsubedi001@gmail.com>
2024-12-04 17:18:57 +0545

    fix: label color fixes in  label

--- src/components/about/Label.tsx
+++ src/components/about/Label.tsx
@@ -1,7 +1,10 @@
+"use client";
 import { FC } from "react";
 
 const Label: FC<{ color: string; title: string }> = ({ color, title }) => {
-  return <div className={`px-3 rounded-md w-fit ${color}`}>{title}</div>;
+  return (
+    <div className={`px-3 rounded-md w-fit ${color} text-white`}>{title}</div>
+  );
 };
 
 export default Label;
