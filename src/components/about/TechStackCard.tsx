c73dd36094aaf10fd600517509122d2eb5a6cdf9
Sachet Subedi <sachetsubedi001@gmail.com>
2024-12-06 21:59:00 +0545

    fix: tech stack icon size changes

--- src/components/about/TechStackCard.tsx
+++ src/components/about/TechStackCard.tsx
@@ -9,10 +9,12 @@ const TechStackCard: FC<{
   return (
     <Card className="border border-none bg-transparent shadow-none">
       <CardContent>
-        <Card className="border-2 border-black dark:border-white rounded-xl w-24 aspect-square flex flex-col items-center justify-center">
-          <Icon icon={icon} className="text-5xl" />
+        <Card className="border-2 border-black dark:border-white rounded-xl w-20 aspect-square flex flex-col items-center justify-center">
+          <Icon icon={icon} className="text-3xl" />
           <CardHeader className="m-0 p-1">
-            <CardTitle className="text-sm font-bold">{title}</CardTitle>
+            <CardTitle className="text-[10px] tracking-widest font-bold">
+              {title}
+            </CardTitle>
           </CardHeader>
         </Card>
       </CardContent>
