b06dae8749ade7e9d284441c211ab209bf963e67
Sachet Subedi <sachetsubedi001@gmail.com>
2024-12-08 21:27:07 +0545

    added links preview

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["api.microlink.io"],
+  },
 };
 
 export default nextConfig;
