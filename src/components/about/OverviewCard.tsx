d8fe8038c0876cb1f51e7132d16caad529fe2c19
Sachet Subedi <sachetsubedi001@gmail.com>
2024-12-09 21:38:05 +0545

    added overview card

--- src/components/about/OverviewCard.tsx
+++ src/components/about/OverviewCard.tsx
@@ -1,5 +1,26 @@
-const OverviewCard = () => {
-  return <div></div>;
+import { CardContent } from "@mui/material";
+import { FC } from "react";
+import { Card } from "../ui/card";
+import Experience from "./Experience";
+
+const OverviewCard: FC<{ hidden: boolean }> = ({ hidden }) => {
+  return (
+    <Card
+      hidden={hidden}
+      className="mt-20 text-justify bg-inherit border-none shadow-none"
+    >
+      <CardContent>
+        <div>
+          I&apos;m <strong> Sachet Subedi</strong>, a full-stack developer
+          passionate about crafting clean, efficient code and building creative
+          digital solutions. From interactive front-end designs to robust
+          back-end systems, I enjoy turning ideas into reality.
+        </div>
+
+        <Experience />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default OverviewCard;
