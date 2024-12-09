6349ee002408dbee5144ceeded6e406be4cecc23
Sachet Subedi <sachetsubedi001@gmail.com>
2024-12-09 23:54:33 +0545

    tab item responsive fixes

--- src/components/about/TabItem.tsx
+++ src/components/about/TabItem.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 
 const TabItem: FC<{ icon: string; label: string }> = ({ icon, label }) => {
   return (
-    <div className="flex gap-2 items-center justify-center">
+    <div className="flex flex-col sm:flex-row gap-2 items-center justify-center">
       <Icon icon={icon} className="text-2xl"></Icon>
       <span>{label}</span>
     </div>
