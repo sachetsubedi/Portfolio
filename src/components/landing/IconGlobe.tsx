1bdba9b42d23a1639f20cb8f1f3761c500c305f4
Sachet Subedi <sachetsubedi001@gmail.com>
2025-01-08 22:58:54 +0545

    fix: icon cloud fixes

--- src/components/landing/IconGlobe.tsx
+++ src/components/landing/IconGlobe.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { globeSlugs } from "@/lib/data";
-import IconCloud from "../ui/icon-cloud";
+import { IconCloud } from "../ui/icon-cloud";
 
 const IconGlobe = () => {
+  const images = globeSlugs.map(
+    (slug) => `https://cdn.simpleicons.org/${slug}/${slug}`
+  );
   return (
     <div>
-      <IconCloud iconSlugs={globeSlugs} />
+      <IconCloud images={images} />
     </div>
   );
 };
