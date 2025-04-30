6a06c25ee10f141187ed5e04341a2e4ff0fc3855
Sachet Subedi <sachetsubedi001@gmail.com>
2025-04-30 18:15:28 +0545

    feat: add slug property to game objects for improved routing

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -2,6 +2,7 @@ export type T_Game = {
   name: string;
   url: string;
   img: string;
+  slug: string;
 };
 
 export type T_Games = T_Game[];
@@ -11,5 +12,12 @@ export const games: T_Games = [
     name: "Simon",
     url: "https://links.sachetsubedi001.com.np/simon",
     img: "/games/simon.png",
+    slug: "simon",
+  },
+  {
+    name: "Flip Cards",
+    url: "https://links.sachetsubedi001.com.np/flip-cards",
+    img: "/games/flip.png",
+    slug: "flip-cards",
   },
 ];
